cecf8d595c8bbc60e5d218f75484dbac4587140f
mufazzal <mamatkhonovmufazzal@gmail.com>
2022-05-01 08:59:43 +0200

    String entered manually

---
+++ ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace reverseString
+{
+    public static class reverseStr
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a string: ");
+            string str  = "John Doe";
+            string reversedString = " ";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedString += str[i];
+            }
+            Console.WriteLine(reversedString);
+        }
+    }
+}
