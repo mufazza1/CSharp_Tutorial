60fbe43545c4aa156e9f9cf47455ae89b8531eba
somebody <somebody@gmail.com>
2022-08-13 15:05:53 +0200

    New projets uploaded

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MultiplicationTable 
+{
+    public class MultTbale 
+    {
+        public static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+            {
+                Console.WriteLine("Number is invalid! ");
+                Console.WriteLine("Valid number is 1 , 2 , 3 ... 10");
+            }
+            if (num < 1)
+            {
+                Console.WriteLine("Number is invalid! ");
+                Console.WriteLine("Valid number is 1 , 2 , 3 ... 10");
+            }
+            if (num > 10)
+            {
+                Console.WriteLine("Number is invalid! ");
+                Console.WriteLine("Valid number is 1 , 2 , 3 ... 10");
+            }
+            else
+            {
+                for(i = 1; i <= num; i++)
+                {
+                    for(j = 1; j <= 10; j++)
+                    {
+                        Console.WriteLine("{0} * {1} = {2}" ,i ,j , i * j);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PasswordAttempts/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class ReverseArray
+{
+    public static void Main()
+    {
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Number {0} =  ", i + 1);
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+        for (int i = 4; i >= 0; i--)
+        {
+            Console.Write("{0} ", numbers[i]);
+        }
+    }
+}
